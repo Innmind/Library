653eefde16ad21e2ae0ad1cfddb3400aaa018f0d
Baptiste Langlade <baptouuuu@gmail.com>
2015-11-23 08:36:39 +0100

    add symfony boilerplate

---
+++ README.md
@@ -0,0 +1,4 @@
+API
+===
+
+Holds crawled page data
---
+++ web/app.php
@@ -0,0 +1,24 @@
+<?php
+
+use Symfony\Component\ClassLoader\ApcClassLoader;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Debug\Debug;
+
+$env = getenv('SYMFONY_ENV') ?: 'prod';
+$debug = $env === 'prod';
+
+$loader = require_once __DIR__.'/../app/bootstrap.php.cache';
+
+if ($debug === true) {
+    Debug::enable();
+}
+
+require_once __DIR__.'/../app/AppKernel.php';
+
+$kernel = new AppKernel('prod', false);
+$kernel->loadClassCache();
+
+$request = Request::createFromGlobals();
+$response = $kernel->handle($request);
+$response->send();
+$kernel->terminate($request, $response);
