edd19862e73549f49dea194a8517778ec690fd3f
Baptiste Langlade <baptouuuu@gmail.com>
2015-11-27 08:08:48 +0100

    add neo4j configuration

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -15,6 +15,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new Innmind\Neo4jBundle\InnmindNeo4jBundle,
             new APIBundle\APIBundle(),
         );
 
