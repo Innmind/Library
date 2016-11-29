5d13a2b7c0855fdf4bc1fc2e21e44fccd3924768
Baptiste Langlade <baptouuuu@gmail.com>
2016-11-29 07:33:54 +0100

    add host name specification

---
+++ tests/Domain/Specification/Host/NameTest.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types = 1);
+
+namespace Tests\Domain\Specification\Host;
+
+use Domain\Specification\Host\Name;
+use Innmind\Specification\ComparatorInterface;
+
+class NameTest extends \PHPUnit_Framework_TestCase
+{
+    public function testInterface()
+    {
+        $spec = new Name('foo');
+
+        $this->assertInstanceOf(ComparatorInterface::class, $spec);
+        $this->assertSame('name', $spec->property());
+        $this->assertSame('=', $spec->sign());
+        $this->assertSame('foo', $spec->value());
+    }
+}
