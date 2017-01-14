f28fcbd05e3e5cee8e29948ce0b17ca4d86b39d9
Baptiste Langlade <baptouuuu@gmail.com>
2017-01-14 16:11:35 +0100

    fix app main controller

--- web/app.php
+++ web/app.php
@@ -1,18 +1,22 @@
 <?php
 
-use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\{
+    HttpFoundation\Request,
+    Debug\Debug
+};
 
 /** @var \Composer\Autoload\ClassLoader $loader */
 $loader = require __DIR__.'/../app/autoload.php';
-include_once __DIR__.'/../var/bootstrap.php.cache';
 
 $env = getenv('SYMFONY_ENV') ?: 'prod';
 
 if ($env === 'dev') {
     Debug::enable();
+} else {
+    include_once __DIR__.'/../var/bootstrap.php.cache';
 }
 
-$kernel = new AppKernel('prod', false);
+$kernel = new AppKernel($env, $env === 'dev');
 $kernel->loadClassCache();
 //$kernel = new AppCache($kernel);
 
