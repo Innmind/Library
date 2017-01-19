5625619ea5c0b98d26c5eca64754091974beb48d
Baptiste Langlade <baptouuuu@gmail.com>
2017-01-19 08:43:38 +0100

    add compiler pass to configure onm data extractor to only use reflection to extract data

--- src/AppBundle/AppBundle.php
+++ src/AppBundle/AppBundle.php
@@ -2,8 +2,18 @@
 
 namespace AppBundle;
 
-use Symfony\Component\HttpKernel\Bundle\Bundle;
+use AppBundle\DependencyInjection\Compiler\ConfigureONMDataExtractorPass;
+use Symfony\Component\{
+    HttpKernel\Bundle\Bundle,
+    DependencyInjection\ContainerBuilder
+};
 
 class AppBundle extends Bundle
 {
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+
+        $container->addCompilerPass(new ConfigureONMDataExtractorPass);
+    }
 }
---
+++ src/AppBundle/DependencyInjection/Compiler/ConfigureONMDataExtractorPass.php
@@ -0,0 +1,32 @@
+<?php
+declare(strict_types = 1);
+
+namespace AppBundle\DependencyInjection\Compiler;
+
+use Symfony\Component\DependencyInjection\{
+    ContainerBuilder,
+    Reference,
+    Compiler\CompilerPassInterface
+};
+
+final class ConfigureONMDataExtractorPass implements CompilerPassInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function process(ContainerBuilder $container)
+    {
+        $container
+            ->getDefinition('innmind_neo4j.entity.data_extractor.aggregate')
+            ->replaceArgument(
+                0,
+                new Reference('onm.data_extractor.strategies')
+            );
+        $container
+            ->getDefinition('innmind_neo4j.entity.data_extractor.relationship')
+            ->replaceArgument(
+                0,
+                new Reference('onm.data_extractor.strategies')
+            );
+    }
+}
