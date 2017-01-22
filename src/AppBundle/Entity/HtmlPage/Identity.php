720eba7974c64fe8429974b73fb439e6b3b8e7f2
Baptiste Langlade <baptouuuu@gmail.com>
2017-01-22 15:35:58 +0100

    make html page and image identities extends the http resource one

--- src/AppBundle/Entity/HtmlPage/Identity.php
+++ src/AppBundle/Entity/HtmlPage/Identity.php
@@ -3,10 +3,10 @@ declare(strict_types = 1);
 
 namespace AppBundle\Entity\HtmlPage;
 
+use AppBundle\Entity\HttpResource\Identity as HttpResourceIdentity;
 use Domain\Entity\HtmlPage\IdentityInterface;
-use Innmind\Neo4j\ONM\Identity\Uuid;
 use Innmind\Rest\Server\IdentityInterface as RestIdentity;
 
-final class Identity extends Uuid implements IdentityInterface, RestIdentity
+final class Identity extends HttpResourceIdentity implements IdentityInterface, RestIdentity
 {
 }
--- src/AppBundle/Entity/HttpResource/Identity.php
+++ src/AppBundle/Entity/HttpResource/Identity.php
@@ -7,6 +7,6 @@ use Domain\Entity\HttpResource\IdentityInterface;
 use Innmind\Neo4j\ONM\Identity\Uuid;
 use Innmind\Rest\Server\IdentityInterface as RestIdentity;
 
-final class Identity extends Uuid implements IdentityInterface, RestIdentity
+class Identity extends Uuid implements IdentityInterface, RestIdentity
 {
 }
