7195e32faeff169d939ab11253ba47e72afd86af
Baptiste Langlade <baptouuuu@gmail.com>
2017-01-22 17:58:39 +0100

    register citation appearance as an onm entity

---
+++ tests/AppBundle/Repository/Neo4j/CitationAppearanceRepositoryTest.php
@@ -0,0 +1,197 @@
+<?php
+declare(strict_types = 1);
+
+namespace Tests\AppBundle\Repository\Neo4j;
+
+use AppBundle\{
+    Repository\Neo4j\CitationAppearanceRepository,
+    Entity\CitationAppearance\Identity
+};
+use Domain\{
+    Repository\CitationAppearanceRepositoryInterface,
+    Entity\CitationAppearance,
+    Entity\Citation\IdentityInterface as CitationIdentity,
+    Entity\HttpResource\IdentityInterface as HttpResourceIdentity,
+    Specification\CitationAppearance\SpecificationInterface
+};
+use Innmind\Neo4j\ONM\RepositoryInterface;
+use Innmind\TimeContinuum\PointInTimeInterface;
+use Innmind\Immutable\{
+    SetInterface,
+    Set
+};
+use Ramsey\Uuid\Uuid;
+
+class CitationAppearanceRepositoryTest extends \PHPUnit_Framework_TestCase
+{
+    public function testInterface()
+    {
+        $this->assertInstanceOf(
+            CitationAppearanceRepositoryInterface::class,
+            new CitationAppearanceRepository(
+                $this->createMock(RepositoryInterface::class)
+            )
+        );
+    }
+
+    public function testGet()
+    {
+        $repository = new CitationAppearanceRepository(
+            $infra = $this->createMock(RepositoryInterface::class)
+        );
+        $identity = new Identity((string) Uuid::uuid4());
+        $infra
+            ->expects($this->once())
+            ->method('get')
+            ->with($identity)
+            ->willReturn(
+                $expected = new CitationAppearance(
+                    $identity,
+                    $this->createMock(CitationIdentity::class),
+                    $this->createMock(HttpResourceIdentity::class),
+                    $this->createMock(PointInTimeInterface::class)
+                )
+            );
+
+        $this->assertSame($expected, $repository->get($identity));
+    }
+
+    public function testAdd()
+    {
+        $repository = new CitationAppearanceRepository(
+            $infra = $this->createMock(RepositoryInterface::class)
+        );
+        $entity = new CitationAppearance(
+            new Identity((string) Uuid::uuid4()),
+            $this->createMock(CitationIdentity::class),
+            $this->createMock(HttpResourceIdentity::class),
+            $this->createMock(PointInTimeInterface::class)
+        );
+        $infra
+            ->expects($this->once())
+            ->method('add')
+            ->with($entity);
+
+        $this->assertSame($repository, $repository->add($entity));
+    }
+
+    public function testRemove()
+    {
+        $repository = new CitationAppearanceRepository(
+            $infra = $this->createMock(RepositoryInterface::class)
+        );
+        $identity = new Identity((string) Uuid::uuid4());
+        $infra
+            ->expects($this->once())
+            ->method('get')
+            ->with($identity)
+            ->willReturn(
+                $entity = new CitationAppearance(
+                    $identity,
+                    $this->createMock(CitationIdentity::class),
+                    $this->createMock(HttpResourceIdentity::class),
+                    $this->createMock(PointInTimeInterface::class)
+                )
+            );
+        $infra
+            ->expects($this->once())
+            ->method('remove')
+            ->with($entity);
+
+        $this->assertSame($repository, $repository->remove($identity));
+    }
+
+    public function testHas()
+    {
+        $repository = new CitationAppearanceRepository(
+            $infra = $this->createMock(RepositoryInterface::class)
+        );
+        $identity = new Identity((string) Uuid::uuid4());
+        $infra
+            ->expects($this->at(0))
+            ->method('has')
+            ->with($identity)
+            ->willReturn(true);
+        $infra
+            ->expects($this->at(1))
+            ->method('has')
+            ->with($identity)
+            ->willReturn(false);
+
+        $this->assertTrue($repository->has($identity));
+        $this->assertFalse($repository->has($identity));
+    }
+
+    public function testCount()
+    {
+        $repository = new CitationAppearanceRepository(
+            $infra = $this->createMock(RepositoryInterface::class)
+        );
+        $infra
+            ->expects($this->once())
+            ->method('all')
+            ->willReturn(
+                $all = $this->createMock(SetInterface::class)
+            );
+        $all
+            ->expects($this->once())
+            ->method('size')
+            ->willReturn(42);
+
+        $this->assertSame(42, $repository->count());
+    }
+
+    public function testAll()
+    {
+        $repository = new CitationAppearanceRepository(
+            $infra = $this->createMock(RepositoryInterface::class)
+        );
+        $infra
+            ->expects($this->once())
+            ->method('all')
+            ->willReturn(
+                (new Set('object'))->add(
+                    $entity = new CitationAppearance(
+                        new Identity((string) Uuid::uuid4()),
+                        $this->createMock(CitationIdentity::class),
+                        $this->createMock(HttpResourceIdentity::class),
+                        $this->createMock(PointInTimeInterface::class)
+                    )
+                )
+            );
+
+        $all = $repository->all();
+
+        $this->assertInstanceOf(SetInterface::class, $all);
+        $this->assertSame(CitationAppearance::class, (string) $all->type());
+        $this->assertSame([$entity], $all->toPrimitive());
+    }
+
+    public function testMatching()
+    {
+        $repository = new CitationAppearanceRepository(
+            $infra = $this->createMock(RepositoryInterface::class)
+        );
+        $specification = $this->createMock(SpecificationInterface::class);
+        $infra
+            ->expects($this->once())
+            ->method('matching')
+            ->with($specification)
+            ->willReturn(
+                (new Set('object'))->add(
+                    $entity = new CitationAppearance(
+                        new Identity((string) Uuid::uuid4()),
+                        $this->createMock(CitationIdentity::class),
+                        $this->createMock(HttpResourceIdentity::class),
+                        $this->createMock(PointInTimeInterface::class)
+                    )
+                )
+            );
+
+        $all = $repository->matching($specification);
+
+        $this->assertInstanceOf(SetInterface::class, $all);
+        $this->assertSame(CitationAppearance::class, (string) $all->type());
+        $this->assertSame([$entity], $all->toPrimitive());
+    }
+}
