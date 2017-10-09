da3b7b58c9ce0676008a2ed6a110ddfcc7f8ce0d
Baptiste Langlade <baptouuuu@gmail.com>
2017-10-09 07:19:10 +0200

    remove the no longer needed bootstrap cache

--- web/app.php
+++ web/app.php
@@ -13,16 +13,9 @@ $env = 'prod';
 
 if ($env === 'dev') {
     Debug::enable();
-} else {
-    include_once __DIR__.'/../var/bootstrap.php.cache';
 }
 
 $kernel = new AppKernel($env, $env === 'dev');
-$kernel->loadClassCache();
-//$kernel = new AppCache($kernel);
-
-// When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
-//Request::enableHttpMethodParameterOverride();
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
